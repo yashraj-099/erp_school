78105acc03cfdfad3e4ca6efcf611597bd4d36bc
yashraj-099 <yashrajdubey1123@gmail.com>
2025-12-25 16:29:35 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py createsuperuser --noinput || true
+python manage.py collectstatic --noinput
