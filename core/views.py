1e249603efdf90e084465da7d6f36040bc9dfcb6
yashraj-099 <yashrajdubey1123@gmail.com>
2025-12-25 16:49:36 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -13,16 +13,16 @@ from django.shortcuts import render
 from reportlab.lib.colors import lightgrey
 
 
-def error_404(request, exception):
+def error_404(request, exception, *args, **kwargsn):
     return render(request, "errors/error_404.html", status=404)
 
-def error_500(request):
+def error_500(request, *args, **kwargs):
     return render(request, "errors/error_500.html", status=500)
 
-def error_403(request, exception=None):
+def error_403(request, exception=None, *args, **kwargs):
     return render(request, "errors/error_403.html", status=403)
 
-def error_400(request, exception=None):
+def error_400(request, exception=None, *args, **kwargs):
     return render(request, "errors/error_400.html", status=400)
 
 
