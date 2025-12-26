479e6f96325eb8958db85333f8630ab149ddc749
yashraj-099 <yashrajdubey1123@gmail.com>
2025-12-26 18:13:41 +0530

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,13 @@
-from django.contrib import admin
-from .models import Role, User, School, Subject, Teacher, Student, FeeRecord, Event
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-class UserAdmin(BaseUserAdmin):
-    pass
-admin.site.register(Role)
-# admin.site.register(User, UserAdmin)
-admin.site.register(School)
-admin.site.register(Subject)
-# admin.site.register(Teacher)
-# admin.site.register(Student)
-admin.site.register(FeeRecord)
-admin.site.register(Event)
+# from django.contrib import admin
+# from .models import Role, User, School, Subject, Teacher, Student, FeeRecord, Event
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# class UserAdmin(BaseUserAdmin):
+#     pass
+# admin.site.register(Role)
+# # admin.site.register(User, UserAdmin)
+# admin.site.register(School)
+# admin.site.register(Subject)
+# # admin.site.register(Teacher)
+# # admin.site.register(Student)
+# admin.site.register(FeeRecord)
+# admin.site.register(Event)
