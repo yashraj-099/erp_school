2d064caa642552cd72023f0e2094b25581eea07b
yashraj-099 <yashrajdubey1123@gmail.com>
2025-12-26 18:40:35 +0530

    Update settings.py

--- school_erp/settings.py
+++ school_erp/settings.py
@@ -12,17 +12,15 @@ ALLOWED_HOSTS = [
     '.onrender.com'
 ]
 
-# AUTH_USER_MODEL = "core.User"
+
 
 INSTALLED_APPS = [
-    # "django.contrib.admin",
+    "django.contrib.admin"
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework",
-    "rest_framework.authtoken",
     
     'core',
     
