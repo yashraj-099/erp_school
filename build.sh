1b84bc1176c6b5c5a0d6f8e134a1a4862c7e4b68
yashraj-099 <yashrajdubey1123@gmail.com>
2025-12-26 17:38:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ pip install -r requirements.txt
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+echo "from django.contrib.auth.models import User; \
+User.objects.filter(username='admin').exists() or \
+User.objects.create_superuser('admin','admin@test.com','Admin@123')" | python manage.py shell
