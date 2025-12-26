a59c48979d8ee2f21a2b3a18c8016a81286cc0fe
yashraj-099 <yashrajdubey1123@gmail.com>
2025-12-26 13:01:37 +0530

    Update settings.py

--- school_erp/settings.py
+++ school_erp/settings.py
@@ -5,7 +5,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 SECRET_KEY = "demo"
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
