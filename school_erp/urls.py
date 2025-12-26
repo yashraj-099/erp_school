f64c62f05caf6d00556137886ccefee8d6398932
yashraj-099 <yashrajdubey1123@gmail.com>
2025-12-26 18:28:14 +0530

    Update urls.py

--- school_erp/urls.py
+++ school_erp/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
-    path("", include("core.auth_urls")),
+    path("", include("core.urls")),
 ]
