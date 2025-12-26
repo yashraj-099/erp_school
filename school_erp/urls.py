8bf8759cf7ec15ad459dc8de43e9b40fd59dc323
yashraj-099 <yashrajdubey1123@gmail.com>
2025-12-26 16:18:23 +0530

    Update urls.py

--- school_erp/urls.py
+++ school_erp/urls.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-# from core import views
-# from django.conf.urls import handler404, handler500, handler403, handler400
-
-# handler404 = "core.views.error_404"
-# # handler500 = "core.views.error_500"
-# handler403 = "core.views.error_403"
-# handler400 = "core.views.error_400"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.auth_urls")),
-    # path('api/', include('core.api_urls')),
-    # path('api-auth/', include('rest_framework.urls')),
-    # path('api/token/', include('core.api_token_urls')),
-    # path('login/', views.login_view, name='login'),
 ]
